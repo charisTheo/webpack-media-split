3bac8f6d90326653bf1801381c553db3bf8cabf6
Charis Theo <dev@charistheo.io>
2021-01-26 18:31:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# webpack-media-split
+
+## Description
+
+A simple example that splits CSS based on media queries used and generates separate files using Webpack.
+Those CSS files are then lazy loaded depending on screen width.
+
+1. Webpack build
+2. Postcss example
+3. `<link media="">` example
+
+## Run dev server
+
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require( "html-webpack-plugin" );
+const MediaQueryPlugin = require('media-query-plugin');
+const path = require( "path" );
+
+module.exports = {
+   context: __dirname,
+   entry: {
+      main: "./src/index.js"
+   },
+   output: {
+      path: path.resolve( __dirname, "dist" ),
+      filename: "[name].js",
+      chunkFilename: "[name].js",
+      publicPath: "/",
+   },
+   module: {
+      rules: [
+         {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+               "style-loader",
+               "css-loader",
+               MediaQueryPlugin.loader,
+               "postcss-loader",
+            ],
+         }
+      ]
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: path.resolve( __dirname, "public/index.html" ),
+         filename: "index.html",
+      }),
+      new MediaQueryPlugin({
+         include: [
+            'styles',
+         ],
+         queries: {
+            'screen and (min-width: 767px)': 'tablets-and-above',
+            'screen and (min-width: 959px)': 'desktop',
+         }
+      })
+   ]
+};
